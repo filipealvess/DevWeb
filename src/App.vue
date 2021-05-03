34e8f1cb69745da592b9d997ea72ad27be91a864
Filipe Alves <filipe.alvesdsouza@gmail.com>
2021-05-03 14:08:21 -0300

    Initial styles

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,61 @@
 <template>
   <div id="app">
+    <Menu></Menu>
 
+    <router-view></router-view>
   </div>
 </template>
 
-<style>
+<script>
+  import Menu from './components/Menu.vue';
 
+  export default {
+    name: 'App',
+    components: {
+      Menu
+    }
+  }
+</script>
+
+<style>
+  html {
+    --dark-black: #181818;
+    --medium-black: #212121;
+    --light-black: #383838;
+    --light-gray: #E0E0E0;
+    --white: #FFFFFF;
+    --blue: #34B1CD;
+
+    font-size: 10px;
+  }
+
+  body {
+    min-height: 300vh;
+    background-color: var(--dark-black);
+  }
+
+  * {
+    padding: 0;
+    margin: 0;
+    border: 0;
+    outline: 0;
+    box-sizing: border-box;
+    color: var(--light-gray);
+    text-decoration: none;
+    font-family: 'Poppins', sans-serif;
+    list-style: none;
+  }
+
+  #app {
+    display: flex;
+  }
+
+  h2, h3 {
+    color: var(--white);
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  button {
+    background-color: transparent;
+  }
 </style>
